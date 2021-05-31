0445fee9c123e0049328f510e5a5470aaea4618b
joansalinas <joan_sz@hotmail.com>
2021-05-31 13:11:13 +0200

    funcion vender, __str__ y calcularPlazasLibres añadida

--- claseBus.py
+++ claseBus.py
@@ -2,3 +2,24 @@ class Bus:
 	def __init__(self, plazas):
 		self.__plazas = plazas
 		self.__plazasOcupadas = 0
+
+	def getPlazasLibres(self):
+		return self.__plazas - self.__plazasOcupadas
+
+	# return plazasLibres
+	def venta(self, billetesVender):
+
+		self.__plazasOcupadas =  if billetesVender>self.getPlazasLibres() else self.__plazasOcupadas + billetesVender
+		return self.getPlazasLibres()
+
+		if billetesVender > self.getPlazasLibres():
+			return self.getPlazasLibres()
+		else:
+			self.__plazasOcupadas += billetesVender
+			return self.getPlazasLibres()
+
+	def __str__(self):
+		return "\n\tPlazas: "+ str(self.__plazas) + "\n\tPlazas libres: "+ str(self.getPlazasLibres())
+bus = Bus(5)
+bus.venta(3)
+print(bus)
