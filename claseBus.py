d9687cff60aeb64365006054d967c17875a10add
Gabrielyanez98 <gabriyanezvallverdu@gmail.com>
2021-05-31 13:17:51 +0200

    Añadida función retorno

--- claseBus.py
+++ claseBus.py
@@ -2,3 +2,12 @@ class Bus:
 	def __init__(self, plazas):
 		self.__plazas = plazas
 		self.__plazasOcupadas = 0
+
+	
+
+	def retorno(self, billetesADevolver):
+    	if(billetesADevolver < self.__plazasOcupadas):
+    		return self.getPlazasLibres()
+		else:
+    		self.__plazasOcupadas -= billetesADevolver
+			return self.getPlazasLibres()
