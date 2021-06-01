6385fe2679c6bcc03a2d4240f8ff22b17d2f6275
JoanSalinas <joan_sz@hotmail.com>
2021-06-01 09:35:59 +0200

    añadido clase billete y cliente

---
+++ billete.py
@@ -0,0 +1,10 @@
+class Billete:
+	def __init__(self, cliente):
+		self.__cliente = cliente
+
+	def getCliente(self):
+		return self.__cliente
+
+	def setCliente(self, cliente)
+		if type(cliente) type
+	
