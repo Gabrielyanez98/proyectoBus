4c69bedae7bdedfa8261d7ea68e6041c28bf1d9c
Gabrielyanez98 <gabriyanezvallverdu@gmail.com>
2021-06-01 09:42:03 +0200

    añadido cliente a clase bus

--- claseBus.py
+++ claseBus.py
@@ -1,31 +1,39 @@
+import Billete
+
 class Bus:
-	def __init__(self, plazas):
+	def __init__(self, plazas, billetes):
 		self.__plazas = plazas
 		self.__plazasOcupadas = 0
+		self.__billetes = billetes
+
 
 	def getPlazasLibres(self):
 		return self.__plazas - self.__plazasOcupadas
 
-	def retorno(self, billetesADevolver):
-		if(billetesADevolver < self.__plazasOcupadas):
+	def retorno(self):
+		if(self.__billetes > self.__plazasOcupadas):
 			return self.getPlazasLibres()
 		else:
-			self.__plazasOcupadas -= billetesADevolver
+			self.__plazasOcupadas -= self.__billetes
 			return self.getPlazasLibres()
+	
+	def venta(self, cliente):
+		
+		
 
-
-	# return plazasLibres
-	def venta(self, billetesVender):
-
-
-		if billetesVender > self.getPlazasLibres():
+		if self.__billetes > self.getPlazasLibres():
 			return self.getPlazasLibres()
 		else:
-			self.__plazasOcupadas += billetesVender
+			self.__plazasOcupadas += self.__billetes
+			billete=Billete(cliente)
+			self.__billetes.append(billete)
 			return self.getPlazasLibres()
 
 	def __str__(self):
 		return "\n\tPlazas: "+ str(self.__plazas) + "\n\tPlazas libres: "+ str(self.getPlazasLibres())
+
 bus = Bus(5)
 bus.venta(3)
+print(bus)
+bus.retorno()
 print(bus)
