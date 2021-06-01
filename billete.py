ee8bc7da5441b0c08bac216aab6787038ff620fd
JoanSalinas <joan_sz@hotmail.com>
2021-06-01 09:51:15 +0200

    clase billete modificada

--- billete.py
+++ billete.py
@@ -1,10 +1,17 @@
+from cliente import Cliente
 class Billete:
 	def __init__(self, cliente):
-		self.__cliente = cliente
+		if type(cliente) is Cliente:
+			self.__cliente = cliente
+		else:
+			self.__cliente = Cliente(cliente)
 
 	def getCliente(self):
 		return self.__cliente
 
-	def setCliente(self, cliente)
-		if type(cliente) type
+	def setCliente(self, cliente):
+		if type(cliente) is Cliente:
+			self.__cliente = cliente
+		else:
+			self.__cliente = Cliente(cliente)
 	
